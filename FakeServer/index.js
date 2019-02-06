654292567f26857817a6859d5dce350a4897496c
Diego Santos <diego.santos@dextra-sw.com>
2019-02-05 23:49:40 -0200

    Added new city to server

--- FakeServer/index.js
+++ FakeServer/index.js
@@ -6,9 +6,15 @@ const Todo = require('./models/todo');
 app.use(express.json()) 
 
 var campinas = {"id": 1, "city_name": "Campinas", "forecast_desc": "Predominantemente ensolarado", "weather": "sunny", "temperature": "28° C", "period_of_the_day": "morning"};
+var japao = {"id": 2, "city_name": "Japão", "forecast_desc": "Trovoadas", "weather": "raining", "temperature": "8° C", "period_of_the_day": "night"};
 app.get('/forecast', function(request, response) {
+
+  delay(1500);
+
   if(request.query.city.toLowerCase() == "campinas") {
     response.status(200).send(campinas)
+  } else if(request.query.city.toLowerCase() == "japao") {
+    response.status(200).send(japao)
   } else {
     response.status(404).send("City not found")
   }
@@ -57,3 +63,8 @@ app.delete('/:id', (request, response) => {
 });
 
 app.listen(port);
+
+function delay(ms) {
+  ms += new Date().getTime();
+  while (new Date() < ms){}
+}
